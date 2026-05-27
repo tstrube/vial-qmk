4037995df89738d73fc9606a6a86365135fa1d84
Timo <76815855+tstrube@users.noreply.github.com>
2026-05-27 20:29:40 +0200

    init

---
+++ keyboards/tsc/tiwaz/keymaps/vial/config.h
@@ -0,0 +1,7 @@
+// Copyright 2026 Timo Strube (@tstrube)
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+#pragma once
+#define VIAL_KEYBOARD_UID {0x13, 0x42, 0xA3, 0xFE, 0x02, 0xC0, 0x31, 0xDB}
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 0 }
+#define VIAL_UNLOCK_COMBO_COLS { 4, 0 }
