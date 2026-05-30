6138782773115c07588bb6155eebc0aeb166559f
Timo <76815855+tstrube@users.noreply.github.com>
2026-05-31 01:06:58 +0200

    improved layout UI

--- keyboards/tsc/tiwaz/keymaps/vial/config.h
+++ keyboards/tsc/tiwaz/keymaps/vial/config.h
@@ -4,4 +4,4 @@
 #pragma once
 #define VIAL_KEYBOARD_UID {0x13, 0x42, 0xA3, 0xFE, 0x02, 0xC0, 0x31, 0xDB}
 #define VIAL_UNLOCK_COMBO_ROWS { 0, 0 }
-#define VIAL_UNLOCK_COMBO_COLS { 4, 0 }
+#define VIAL_UNLOCK_COMBO_COLS { 1, 0 }
